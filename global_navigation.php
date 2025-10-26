f70c3d25119d6e9feb47531cbb6f60cffcf55684
Dhruv Vira <dhruvvira007@gmail.com>
2025-10-26 14:41:21 +0530

    Populate user stats in the global navigation

--- global_navigation.php
+++ global_navigation.php
@@ -1,8 +1,55 @@
 <?php
-// Get the current script's file name (e.g., 'dashboard.php', 'skill_tree.php')
-// $_SERVER is a PHP superglobal array that contains information about the server and execution environment.
-// $_SERVER['PHP_SELF'] holds the path and file name of the current script, relative to the document root of your web server.
-// basename() is a built-in PHP function that returns the trailing name component of a path. In simple terms, it extracts only the file name from the full path string.
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+$user_id = $_SESSION['user_id'] ?? null;
+$star_points = '???';
+$skill_points = '???';
+$username = 'Guest';
+
+// DATABASE CONFIGURATION
+$DB_HOST = 'localhost';
+$DB_USER = 'root';
+$DB_PASS = '';
+$DB_NAME = 'coinflow_academy_db'; 
+
+if ($user_id) {
+    $mysqli = @new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+
+    // Check connection
+    if ($mysqli->connect_errno) {
+        error_log("Failed to connect to MySQL: " . $mysqli->connect_error);
+    } else {
+        // --- QUERY A: Get Currency Stats ---
+        $stmt_stats = $mysqli->prepare("SELECT star_points, skill_points FROM User_Stats WHERE user_id = ?");
+        $stmt_stats->bind_param("i", $user_id); 
+        $stmt_stats->execute();
+        
+        $stmt_stats->bind_result($fetched_star_points, $fetched_skill_points);
+        
+        if ($stmt_stats->fetch()) {
+            $star_points = number_format($fetched_star_points);
+            $skill_points = number_format($fetched_skill_points);
+        }
+        $stmt_stats->close();
+
+
+        // --- QUERY B: Get Username from Users table ---
+        $stmt_user = $mysqli->prepare("SELECT username FROM Users WHERE user_id = ?");
+        $stmt_user->bind_param("i", $user_id);
+        $stmt_user->execute();
+        $stmt_user->bind_result($fetched_username);
+        
+        if ($stmt_user->fetch()) {
+            $username = htmlspecialchars($fetched_username);
+        }
+        $stmt_user->close();
+        
+        $mysqli->close();
+    }
+}
+
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 
@@ -25,13 +72,13 @@ $current_page = basename($_SERVER['PHP_SELF']);
             <ul class="navbar-nav">
                 <li class="nav-item me-3 position-relative">
                     <a class="nav-link custom-currency-link d-flex align-items-center" href="#">
-                        <img class="currency_icon" src="../images/star_point_symbol.png"> <span class="currency-value">1000</span>
+                        <img class="currency_icon" src="../images/star_point_symbol.png"> <span class="currency-value"><?php echo $star_points; ?></span>
                         <div class="currency-tooltip">Star Points</div>
                     </a>
                 </li>
                 <li class="nav-item position-relative">
                     <a class="nav-link custom-currency-link d-flex align-items-center" href="#">
-                        <img class="currency_icon" src="../images/skill_point_symbol.png"> <span class="currency-value">1000</span>
+                        <img class="currency_icon" src="../images/skill_point_symbol.png"> <span class="currency-value"><?php echo $skill_points; ?></span>
                         <div class="currency-tooltip">Skill Points</div>
                     </a>
                 </li>   
@@ -42,7 +89,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 
 <div class="offcanvas offcanvas-start custom-offcanvas-bg" tabindex="-1" id="mainMenuOffcanvas" aria-labelledby="mainMenuOffcanvasLabel">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="mainMenuOffcanvasLabel">Academy Hub</h5>
+        <h5 class="offcanvas-title" id="mainMenuOffcanvasLabel">Welcome, <?php echo $username; ?></h5>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
 
